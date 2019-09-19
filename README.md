bc99ba59fda227a9eb678b58c74e0da89c244243
Roman-Ast <roman_planeta@mail.ru>
2019-09-19 19:59:07 +0500

    code quality step 4

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 My first hexlet-project-lvl1
+
+<a href="https://codeclimate.com/github/Roman-Ast/php-project-lvl1/maintainability"><img src="https://api.codeclimate.com/v1/badges/3664cee03b2c64b323f1/maintainability" /></a>
+
+<a href="https://codeclimate.com/github/Roman-Ast/php-project-lvl1/test_coverage"><img src="https://api.codeclimate.com/v1/badges/3664cee03b2c64b323f1/test_coverage" /></a>
--- src/cli.php
+++ src/cli.php
@@ -1,14 +1,31 @@
 <?php
 
+/**
+ * CLI functions to interraction with user
+ *
+ * PHP version 7.2
+ *
+ * @category PHP
+ * @package  Php-project-lvl1
+ * @author   Popadinets Roman <roman_planeta@mail.ru>
+ * @license  https://github.com/Roman-Ast/php-project-lvl1 MIT
+ * @link     https://github.com/Roman-Ast/php-project-lvl1
+ */
+
+
 namespace BrainGames\Cli;
 
 use function cli\line;
 use function cli\prompt;
 
+/**
+ * This function start this project
+ *
+ * @return void
+ */
 function run()
 {
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     return $name;
 }
-
