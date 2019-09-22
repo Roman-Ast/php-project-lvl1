bd92756e2f3e13a12f9eb61cc6b6293eacad4042
Roman-Ast <roman_planeta@mail.ru>
2019-09-22 16:58:38 +0500

    fix lint notifications

--- src/gcd.php
+++ src/gcd.php
@@ -1,10 +1,30 @@
 <?php
 
+/**
+ * CLI function to play game with users
+ *
+ * PHP version 7.2
+ *
+ * @category PHP
+ * @package  Php-project-lvl1
+ * @author   Popadinets Roman <roman_planeta@mail.ru>
+ * @license  https://github.com/Roman-Ast/php-project-lvl1 MIT
+ * @link     https://github.com/Roman-Ast/php-project-lvl1
+ */
+
 namespace BrainGames\Cli;
 
 use function cli\line;
 use function cli\prompt;
 
+/**
+ * This function is to find GCD of two numbers
+ *
+ * @param integer $n - is the first number
+ * @param integer $m - is the second number
+ * 
+ * @return gcd;
+ */
 function findGcd($n, $m)
 {
     if ($m > 0) {
@@ -14,12 +34,28 @@ function findGcd($n, $m)
     }
 }
 
-function hasGcd($num1, $num2)
+/**
+ * This function is to check does two numbers have common GCD
+ *
+ * @param integer $n - is the first number
+ * @param integer $m - is the second number
+ * 
+ * @return gcd;
+ */
+function hasGcd($n, $m)
 {
-    $result = findGcd($num1, $num2);
+    $result = findGcd($n, $m);
     return is_int($result) && $result > 1;
 }
 
+/**
+ * This function to interract with users
+ *
+ * @param string  $user  - is name of user, recieved from STDIN
+ * @param integer $round - is count of rounds completed by user
+ *
+ * @return game or void;
+ */
 function gameGcd($user, $round = 1)
 {
     $random1 = rand(0, 100);
